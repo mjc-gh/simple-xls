873d993d57eab707a7e0f1792482aa57bdf33188
mikeycgto <mikeycgto@gmail.com>
2011-07-08 19:53:50 -0400

    add in another test for output string

--- test/test_simple_xls.rb
+++ test/test_simple_xls.rb
@@ -6,14 +6,14 @@ class SimpleXlsTest < Test::Unit::TestCase
     headers = ['a', 'b', 'c']
     xls = SimpleXLS.new(headers)
     
-    assert_equal(headers, xls.headers)
+    assert_equal headers, xls.headers
   end
   
   def test_initialize_dups_headers
     headers = ['a', 'b', 'c']
     xls = SimpleXLS.new(headers)
     
-    assert_not_same(headers, xls.headers)
+    assert_not_same headers, xls.headers
   end
   
   def test_row_push
@@ -22,7 +22,7 @@ class SimpleXlsTest < Test::Unit::TestCase
     
     xls.push(row)
     
-    assert_equal(row, xls.rows.first)
+    assert_equal row, xls.rows.first
   end
   
   def test_row_push_dups_row
@@ -31,6 +31,20 @@ class SimpleXlsTest < Test::Unit::TestCase
     
     xls.push(row)
     
-    assert_not_same(row, xls.rows.first)
+    assert_not_same row, xls.rows.first
+  end
+  
+  def test_output
+    xls = SimpleXLS.new(['a', 'b', 'c'])
+    xls.push([1,2,3])
+    
+    output = xls.to_s
+    
+    assert output.include?('<table><thead>')
+    assert output.include?('<th>a</th><th>b</th><th>c</th>')
+    assert output.include?('<td>1</td><td>2</td><td>3</td>')
+    assert output.include?('</tbody></table>')
+
+    assert_equal 2, output.scan(/<tr>/).size
   end
 end

283dedf76f203ac002abba11aa7e43a1f14b4d55
mikeycgto <mikeycgto@gmail.com>
2012-02-29 11:34:27 -0500

    add new tests

--- test/test_simple_xls.rb
+++ test/test_simple_xls.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../lib/simple-xls.rb', __FILE__)
+require 'simplexls'
 require 'test/unit'
 
 class SimpleXlsTest < Test::Unit::TestCase
@@ -25,6 +25,22 @@ class SimpleXlsTest < Test::Unit::TestCase
     assert_equal row, xls.rows.first
   end
   
+  def test_row_push_alias
+     xls = SimpleXLS.new(['a','b','c'])
+    
+     assert_equal xls.method(:push), xls.method(:<<)
+  end
+  
+  def test_evens_out_row
+    xls = SimpleXLS.new(['a','b','c'])
+    row = [1,2]
+    
+    xls.push(row)
+    
+    assert_equal 3, xls.rows.first.size
+    assert_equal [1,2,nil], xls.rows.first
+  end
+  
   def test_row_push_dups_row
     xls = SimpleXLS.new(['a', 'b', 'c'])
     row = [1,2,3]
@@ -32,6 +48,32 @@ class SimpleXlsTest < Test::Unit::TestCase
     xls.push(row)
     
     assert_not_same row, xls.rows.first
+  end
+  
+  def test_add_collection
+    xls = SimpleXLS.new(['a', 'b', 'c'])
+    rows = [[1,2,3], [4,5,6]]
+    
+    xls.add(rows)
+    
+    assert_equal rows.first, xls.rows.first
+    assert_equal rows.last, xls.rows.last
+  end
+  
+  def test_add_collection_with_hash
+    xls = SimpleXLS.new(['a', 'b', 'c'])
+    
+    rows = [{:a => 1, :b => 2, :c => 3}, {:a => 4, :b => 5, :d => 7}]
+    xls.add(rows)
+    
+    assert_equal rows.first.values, xls.rows.first
+    assert_equal [4,5,nil], xls.rows.last
+    
+    rows = [{:a => 1, :b => 2, :c => 3}, {:a => 4, :c => 5, :d => 7}]
+    xls.add(rows)
+    
+    assert_equal rows.first.values, xls.rows.first
+    assert_equal [4,nil,5], xls.rows.last    
   end
   
   def test_output

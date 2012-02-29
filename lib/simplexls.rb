e3cc4152e0bb920e81a39e266be2ca91d872f861
mikeycgto <mikeycgto@gmail.com>
2012-02-29 11:34:21 -0500

    updates to code; add new method

--- lib/simplexls.rb
+++ lib/simplexls.rb
@@ -2,25 +2,35 @@ class SimpleXLS
   attr_accessor :headers, :rows
   
   def initialize(headers)
-    raise ArugmentError('Row must be an Array') unless headers.is_a?(Array)
+    raise ArgumentError.new('headers must be an Array') unless Array === headers
 
-    @headers = headers.nil? ? [] : headers.dup
+    @headers = headers.dup
     @rows = []
   end
-  
+  # TODO fill out array here is needed rather than when we build the string
+  #      also, slice the array is longer than headers
   def push(row)
-    raise ArugmentError('Row must be an Array') unless row.is_a?(Array)
-    @rows.push(row.dup)
+    raise ArgumentError.new('row must be an Array') unless Array === row
+    
+    row = row.dup
+    row += Array.new(headers.size - row.size) if row.size < headers.size
+    
+    @rows.push row
+  end
+
+  alias :<< :push
+  
+  # TODO test this!
+  def add(collection)
+    collection.each do |obj|
+      self << (Hash === obj ? headers.map { |key| obj[key] || obj[key.to_sym] } : obj.to_a)
+    end
   end
   
   def to_s
     output = "<table><thead><tr>"
     output << @headers.collect { |hr| "<th>#{hr}</th>" }.join << "</tr></thead><tbody>"
-    output << @rows.collect { |row|
-      # even out the row if needed
-      row += Array.new(headers.size - row.size) if row.size != headers.size
-      "<tr>#{row.collect { |cell| "<td>#{cell}</td>"}.join}</tr>"
-    }.join
+    output << @rows.collect { |row| "<tr>#{row.collect { |cell| "<td>#{cell}</td>"}.join}</tr>" }.join
     output << "</tbody></table>"
   end
 end
